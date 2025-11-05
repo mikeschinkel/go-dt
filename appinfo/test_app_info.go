b44f91daac5d4392a1371540417b237f1bf76542
Mike Schinkel <mike@newclarity.net>
2025-11-05 15:35:31 -0500

    chore: Delete old code

--- appinfo/test_app_info.go
+++ appinfo/test_app_info.go
@@ -16,12 +16,10 @@ func (ai *Test) SetExtraInfo(info map[string]any) {
 	ai.extraInfo = info
 }
 
-//	func (ai *Test) SetConfigPath(cd dt.PathSegments) {
-//		ai.configPath = cd
-//	}
 func (ai *Test) SetConfigFile(cf dt.RelFilepath) {
 	ai.configFile = cf
 }
+
 func (ai *Test) SetLogFile(lf dt.Filename) {
 	ai.logFile = lf
 }
