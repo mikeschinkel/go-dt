e8473f79220cf56c54b8eaff351fc28afe2b4f85
Mike Schinkel <mike@newclarity.net>
2025-11-05 15:34:47 -0500

    feat: Add `DirPathJoin4..5()`

--- join.go
+++ join.go
@@ -12,6 +12,14 @@ func DirPathJoin3[T1, T2, T3 ~string](a T1, b T2, c T3) DirPath {
 	return DirPath(filepath.Join(string(a), string(b), string(c)))
 }
 
+func DirPathJoin4[T1, T2, T3, T4 ~string](a T1, b T2, c T3, d T4) DirPath {
+	return DirPath(filepath.Join(string(a), string(b), string(c), string(d)))
+}
+
+func DirPathJoin5[T1, T2, T3, T4, T5 ~string](a T1, b T2, c T3, d T4, e T5) DirPath {
+	return DirPath(filepath.Join(string(a), string(b), string(c), string(d), string(e)))
+}
+
 func FilepathJoin[T1, T2 ~string](a T1, b T2) Filepath {
 	return Filepath(filepath.Join(string(a), string(b)))
 }
